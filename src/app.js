dabb1c442465deda2876e48b391871173c1599d7
hfvmarques <hfvmarques@gmail.com>
2022-05-13 09:17:01 -0400

    organizes routes

--- src/app.js
+++ src/app.js
@@ -3,19 +3,12 @@ const consign = require('consign');
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/middleware.js')
+  .then('./routes')
+  .then('./config/routes.js')
   .into(app);
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
-});
-
-app.get('/users', (req, res) => {
-  const users = [{ name: 'John Doe', email: 'john@doe.com' }];
-  res.status(200).json(users);
-});
-
-app.post('/users', (req, res) => {
-  res.status(201).json(req.body);
 });
 
 module.exports = app;
