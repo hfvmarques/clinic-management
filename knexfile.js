f6da073401088feb1126a3c0223940da801f319d
hfvmarques <hfvmarques@gmail.com>
2022-05-13 21:28:49 -0400

    adds pg, knex and users migration

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+  test: {
+    client: 'pg',
+    version: '9.6',
+    connection: {
+      host: 'localhost',
+      user: 'postgres',
+      password: 'postgres',
+      database: 'clinic_management',
+    },
+    migrations: {
+      directory: './src/migrations',
+    },
+  },
+};
---
+++ src/migrations/20220514011601_create_users.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) =>
+  knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.string('email').notNullable().unique();
+    table.string('password').notNullable();
+    table.timestamps(true, true);
+  });
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => knex.schema.dropTable('users');
