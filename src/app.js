fb77e882b392620fbea6bbfe512039344e4ff068
hfvmarques <hfvmarques@gmail.com>
2022-06-15 15:12:50 -0400

    adds logs treatment

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
+const winston = require('winston');
+const uuid = require('uuidv4');
 const knexfile = require('../knexfile');
 
 app.db = knex(knexfile[process.env.NODE_ENV]);
+
+app.log = winston.createLogger({
+  level: 'debug',
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.json({ space: 1 }),
+    }),
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'warn',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json({ space: 1 })
+      ),
+    }),
+  ],
+});
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/passport.js')
@@ -20,7 +39,16 @@ app.get('/', (req, res) => {
 app.use((err, req, res, next) => {
   const { name, message, stack } = err;
   if (name === 'ValidationError') res.status(400).json({ error: message });
-  else res.status(500).json({ name, message, stack });
+  else {
+    const id = uuid();
+    app.log.error({
+      id,
+      name,
+      message,
+      stack,
+    });
+    res.status(500).json({ id, error: 'Internal error.' });
+  }
   next(err);
 });
 
