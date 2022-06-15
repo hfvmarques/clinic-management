bcfdede7cd52fa93910ade851cb61890235b2d13
hfvmarques <hfvmarques@gmail.com>
2022-06-14 22:47:39 -0400

    prepares api for prod environment

--- knexfile.js
+++ knexfile.js
@@ -11,4 +11,26 @@ module.exports = {
     migrations: { directory: './src/migrations' },
     seeds: { directory: './src/seeds' },
   },
+  prod: {
+    client: 'pg',
+    version: '9.6',
+    connection: {
+      host: 'localhost',
+      user: 'postgres',
+      password: 'postgres',
+      database: 'prod_clinic_management',
+    },
+    migrations: { directory: './src/migrations' },
+  },
+  development: {
+    client: 'pg',
+    version: '9.6',
+    connection: {
+      host: 'localhost',
+      user: 'postgres',
+      password: 'postgres',
+      database: 'dev_clinic_management',
+    },
+    migrations: { directory: './src/migrations' },
+  },
 };
